b42f034f9bf63fc9ebf96c88de300fe0830ad852
Daria Lykova <daria.lykova@gmail.com>
2019-09-04 15:18:31 +0200

    Creates a fixture file for flashcard dummy data/Calls for it in FlashCardTemplate in order to pass the tests

--- src/components/FlashCardTemplate.js
+++ src/components/FlashCardTemplate.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Reveal, Card } from 'semantic-ui-react';
 
 const FlashCardTemplate = (props) => {
   const flashcard = props.flashcard;
-    return (
-      <Container>
+  return (
+    <Container>
+      <div id={`flashcard_${flashcard.id}`}>
         <Reveal animated='fade'>
           <Reveal.Content visible>
-            <Card style={{width: 400, height: 400}}>
-              <Card.Header style={{marginTop: '10em'}} textAlign='center'>
-                Question
+            <Card style={{ width: 400, height: 400 }}>
+              <Card.Header style={{ marginTop: '10em' }} textAlign='center'>
+                {flashcard.question}
               </Card.Header>
             </Card>
           </Reveal.Content>
           <Reveal.Content hidden>
-            <Card style={{width: 400, height: 400}}>
-              <Card.Header style={{marginTop: '10em'}} textAlign='center'>
-                Answer
+            <Card style={{ width: 400, height: 400 }}>
+              <Card.Header style={{ marginTop: '10em' }} textAlign='center'>
+                {flashcard.answer}
               </Card.Header>
             </Card>
           </Reveal.Content>
         </Reveal>
-      </Container>
-    )
+      </div>
+    </Container>
+  )
 }
 
 export default FlashCardTemplate;
