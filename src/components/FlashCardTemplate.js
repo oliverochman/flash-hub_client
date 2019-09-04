4ea6a4120c32bf6a1ba5379cfabf7aa936aed034
Julie Ahlquist <julieahlquist@hotmail.com>
2019-09-04 18:34:22 +0200

    imports axios, changes to Class and makes attempt to use componentDidMount

--- src/components/FlashCardTemplate.js
+++ src/components/FlashCardTemplate.js
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Reveal, Card } from 'semantic-ui-react';
+import axios from 'axios';
 
-const FlashcardTemplate = (props) => {
-  const flashcard = props.flashcard;
+class FlashcardTemplate extends Component {
+  state = {
+    id: '',
+    question: '',
+    answer: ''
+  };
+
+  // componentDidMount() {
+  //   this.getFlashcard();
+  // }
+
+  // async getFlashcard() {
+  //   const response = await axios.get('/flashcard');
+  //   if (response.data.length > 0) {
+  //     this.setState({
+  //       flashcard: response.FlashcardTemplate
+  //     });
+  //   }
+  // }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/flash_cards').then(response => {
+      this.setState({ articles: response.data.flashcard });
+    });
+  }
+
+  // componentDidMount() {
+  //   let flashcardPath;
+  //   axios.get(flashcardPath).then(response => {
+  //   this.setState({
+  //     id: response.data.id,
+  //     question: response.data.question,
+  //     answer: response.data.answer
+  //   });
+  //   })
+  // }
+
+  render() {
+    let flashcard;
+    
   return (
     <Container>
       <div id={`flashcard_${flashcard.id}`}>
         <Reveal animated='fade'>
           <Reveal.Content visible>
             <Card style={{ width: 400, height: 400 }}>
-              <Card.Header style={{ marginTop: '10em' }} textAlign='center'>
+              <Card.Header id="question" style={{ marginTop: '10em' }} textAlign='center'>
                 {flashcard.question}
               </Card.Header>
             </Card>
           </Reveal.Content>
           <Reveal.Content hidden>
             <Card style={{ width: 400, height: 400 }}>
-              <Card.Header style={{ marginTop: '10em' }} textAlign='center'>
+              <Card.Header id="answer" style={{ marginTop: '10em' }} textAlign='center'>
                 {flashcard.answer}
               </Card.Header>
             </Card>
@@ -26,5 +65,7 @@ const FlashcardTemplate = (props) => {
     </Container>
   )
 }
+}
+
 
 export default FlashcardTemplate;
