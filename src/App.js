788830be6aeea9272b35f5ab03dbd8f6fec7393b
Julie Ahlquist <julieahlquist@hotmail.com>
2019-09-07 19:07:48 +0200

    deletes button component, changes status module to try catch, modifies app functions and changes to form
    Co-authored-by: Daria Lykova <daria.lykova@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,33 +2,39 @@ import React, { Component } from 'react';
 import Flashcard from './components/Flashcard';
 import axios from 'axios';
 import './styling/status-buttons.css';
+import { updateFlashcardStatus } from './modules/updateFlashcardStatus'
 
 class App extends Component {
   state = {
     flashcards: [],
-    status: 'no_status',
+    statusUpdated: false
   }
+  
 
-  async componentDidMount() {
-    let response = await axios.get('http://localhost:3000/api/flashcards')
-      this.setState({
-        flashcards: response.data
+  async componentDidMount () {
+    const response = await axios.get('http://localhost:3000/api/flashcards')
+    this.setState({
+      flashcards: response.data
     })
   };
 
-  updateStatusHandler = () => {
-    axios.put('/api/flashcards/${this.props.id}')
+  async updateGreenStatus() {
+    let response = await updateFlashcardStatus(
+      this.props.status
+    )
     .then(() => {
       this.setState({
-        status: 'green'
-      })
+        statusUpdated: true,
+        status: response.data.status
+      });
+    console.log('update green state')
     })
-    console.log('update state')
   }
 
   render() {
     const flashcards = this.state.flashcards
     let flashcardDisplay 
+    let statusMessage
 
     if (flashcards.length >= 1) {
       flashcardDisplay = <Flashcard flashcard={flashcards[0]} key={flashcards[0].id} />
@@ -40,15 +46,59 @@ class App extends Component {
     //   };
     // };
 
+    if (this.props.statusUpdated === false) {
+      statusMessage = (
+      <>
+        <p>Successfully added status</p>
+      </>
+      )
+    } else if (this.props.statusUpdated === true) {
+      statusMessage= (
+      <>
+        <p>No status</p>
+      </>
+      )
+    };
+
     return (
       <>
         <h1>FlashHub</h1>
         {flashcardDisplay}
+        {statusMessage}
         
-        <form className='button-group' onSubmit={(e) => this.updateStatusHandler(e)}>
-          <input type='radio' className='update-button' id='red'>Repeat, please</input>
-          <input type='radio' className='update-button' id='yellow'>Needs more practice</input>
-          <input type='radio' className='update-button' id='green'>I got this!</input>
+        {/* <div className='button-group'>
+          <button className='update-button' id='red'>Repeat, please</button>
+          <button className='update-button' id='yellow'>Needs more practice</button>
+          <button className='update-button' onClick={this.updateGreenStatus.bind(this)} id='green'>I got this!</button>
+        </div> */}
+
+        <form className='button-group'>
+          <label>
+            <input type='radio' 
+                   name='update' 
+                   className='update-button' 
+                   id='red'
+                   onChange={e => this.setState({ red: e.target.value })}/>
+                   Repeat, please
+          </label>
+
+          <label>
+            <input type='radio' 
+                   name='update' 
+                   className='update-button' 
+                   id='yellow' 
+                   onChange={e => this.setState({ yellow: e.target.value })}/>
+                   Needs more practice
+          </label>
+
+          <label>
+            <input type='radio' 
+                   name='update' 
+                   className='update-button' 
+                   id='green'
+                   onClick={this.updateGreenStatus.bind(this)} onChange={e => this.setState({ green: e.target.value })}/>
+                   I got this!
+          </label>
         </form>
       </>
     )
--- src/modules/updateFlashcardStatus.js
+++ src/modules/updateFlashcardStatus.js
@@ -1,26 +1,15 @@
 import axios from 'axios';
 
-//Move this too App.js component?
-const apiUrl = 'http://localhost:3000/api/flashcards';
-
-const updateGreenStatus = async (status) => {
-
-  let headers = await sessionStorage.getItem('status');
-    headers = JSON.parse(headers);
-    headers =  {
-      ...headers,
-      "Content-type": "application/json",
-      Accept: "application/json"
-    };
-  const path = apiUrl + `${flashcard.id}`;
-  return new Promise((resolve, response) => {
-    axios.put(path, {
-      headers: headers
-    })
-    .then(response => {
-      resolve(response);
-    });
-  });
+const updateFlashcardStatus = async (status) => {
+  try {
+    let response = await axios.put('http://localhost:3000/api/flashcards/1' , {
+        statusUpdated: true,
+      });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
 };
 
-export { updateGreenStatus }
+export { updateFlashcardStatus }
+
