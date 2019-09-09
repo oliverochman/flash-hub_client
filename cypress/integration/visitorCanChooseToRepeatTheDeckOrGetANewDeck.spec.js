f1ae6df62c2e5d923932ac20c1cb1d31ac5f10b8
oliverochman <oliver.ochman@hotmail.com>
2019-09-09 16:51:44 +0200

    add assertion to feature test and adds second fixture file for flashcards
    Co-authored-by: julieahlquist <julieahlquist@hotmail.com>
    Co-authored-by: dashlykova <daria.lykova@gmail.com>

--- cypress/integration/visitorCanChooseToRepeatTheDeckOrGetANewDeck.spec.js
+++ cypress/integration/visitorCanChooseToRepeatTheDeckOrGetANewDeck.spec.js
@@ -7,6 +7,20 @@ describe('Visitor can choose to repeat the deck or get a new deck', () => {
       response: 'fixture:flashcards.json',
       status: 200
     });
+
+    cy.route({
+      method: 'PUT',
+      url: 'http://localhost:3000/api/flashcards/**',
+      response: 'fixture:successful_update_flashcard_status.json',
+      status: 200
+    });
+
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/api/decks/**',
+      response: 'fixture:flashcards_second_page.json',
+      status: 200
+    });
     cy.visit('http://localhost:3001');
   });
 
@@ -14,9 +28,13 @@ describe('Visitor can choose to repeat the deck or get a new deck', () => {
     it('successfully', () => {
       for(let n = 0; n < 10; n ++){
         cy.get('#green').click()
+        cy.wait(500)
       }
 
       cy.get('#get-new-deck').click()
+
+      cy.get('#question_1').contains('Quotes used with string interpolation');
+      cy.get('#answer_1').contains("# double quote");
     })
   });
 
@@ -24,9 +42,13 @@ describe('Visitor can choose to repeat the deck or get a new deck', () => {
     it('successfully', () => {
       for(let n = 0; n < 10; n ++){
         cy.get('#green').click()
+        cy.wait(500)
       }
 
       cy.get('#repeat-deck').click()
+
+      cy.get('#question_1').contains('How can you include an external javascript file?');
+      cy.get('#answer_1').contains("/script src='myfile.js'/");
     })
   })
 });
