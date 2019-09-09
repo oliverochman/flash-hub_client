1523b8b24d718708802e1c1b4a715f82ae699e3b
oliverochman <oliver.ochman@hotmail.com>
2019-09-09 16:52:36 +0200

    refactor flash update module
    Co-authored-by: julieahlquist <julieahlquist@hotmail.com>
    Co-authored-by: dashlykova <daria.lykova@gmail.com>

--- src/modules/updateFlashcardStatus.js
+++ src/modules/updateFlashcardStatus.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 
 const updateFlashcardStatus = async (status, id) => {
   try {
-    let response = await axios.put(`http://localhost:3000/api/flashcards/${id}`, 
-    {
-      status: status
-    });
+    let response = await axios.put(`http://localhost:3000/api/flashcards/${id}/?status=${status}`);
     return response;
   } catch (error) {
     return error.response;
