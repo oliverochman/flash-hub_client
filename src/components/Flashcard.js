887208873c27f838f7386fae31a494be45c8b560
Julie Ahlquist <julieahlquist@hotmail.com>
2019-09-09 20:03:25 +0200

    wraps elements in grids and moves answer text

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,37 +1,43 @@
 import React from 'react';
 import "../styling/status-buttons.css";
-import { Reveal, Card, Button } from 'semantic-ui-react';
+import { Reveal, Card, Button, Grid } from 'semantic-ui-react';
 
 const Flashcard = (props) => {
   let flashcard = props.flashcard;
   return (
     <>
-      <Reveal animated='fade' id={`id_${flashcard.id}`}>
-        <Reveal.Content visible>
-          <Card style={{ width: 580, height: 400 }}>
-            <Card.Header as="h1" className="ui orange header" id={`question_${flashcard.id}`} style={{ marginTop: '5em' }} textAlign='center'>
-            {flashcard.question}
-            </Card.Header>
-          </Card>
-        </Reveal.Content>
-        <Reveal.Content>
-          <Card style={{ width: 580, height: 400 }}>
-            <Card.Header as="h3" className="ui black header" id={`answer_${flashcard.id}`} style={{ marginTop: '7em', marginLeft: '1em', marginRight: '1em'}} textAlign='center'>
-            {flashcard.answer}
-            </Card.Header>
-          </Card>
-        </Reveal.Content>
-      </Reveal>
-
-      <Button className='update-button' onClick={props.updateStatus} id='red' style={{ width: 191, height: 50 }}>
-        Repeat, please
-      </Button>
-      <Button className='update-button' onClick={props.updateStatus} id='yellow' style={{ width: 191, height: 50 }}>
-        Needs more practice
-      </Button>
-      <Button className='update-button' onClick={props.updateStatus} id='green' style={{ width: 191, height: 50 }}>
-        I got this
-      </Button>
+      <Grid centered columns={2}>
+        <Grid.Column width={11}>
+          <Reveal animated='fade' id={`id_${flashcard.id}`}>
+            <Reveal.Content visible>
+              <Card style={{ width: 580, height: 400 }}>
+                <Card.Header as="h1" className="ui orange header" id={`question_${flashcard.id}`} style={{ marginTop: '5em' }} textAlign='center'>
+                {flashcard.question}
+                </Card.Header>
+              </Card>
+            </Reveal.Content>
+            <Reveal.Content>
+              <Card style={{ width: 580, height: 400 }}>
+                <Card.Header as="h3" className="ui black header" id={`answer_${flashcard.id}`} style={{ marginTop: '9em', marginLeft: '1em', marginRight: '1em'}} textAlign='center'>
+                {flashcard.answer}
+                </Card.Header>
+              </Card>
+            </Reveal.Content>
+          </Reveal>
+          </Grid.Column>
+          
+          <Grid.Column width={11}>
+          <Button className='update-button' onClick={props.updateStatus} id='red' style={{ width: 191, height: 50 }}>
+            Repeat, please
+          </Button>
+          <Button className='update-button' onClick={props.updateStatus} id='yellow' style={{ width: 191, height: 50 }}>
+            Needs more practice
+          </Button>
+          <Button className='update-button' onClick={props.updateStatus} id='green' style={{ width: 191, height: 50 }}>
+            I got this
+          </Button>
+        </Grid.Column>
+      </Grid>
     </>
   )
 };
