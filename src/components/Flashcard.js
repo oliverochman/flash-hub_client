30d0d633a6937ce56d447be117fbe26df2b2fa1e
Julie Ahlquist <julieahlquist@hotmail.com>
2019-09-09 19:15:12 +0200

    adds href tags, changes card styling to pure semantic and buttons to semantic

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,30 +1,39 @@
 import React from 'react';
-import '../styling/flashcard.css';
+// import '../styling/flashcard.css';
 import "../styling/status-buttons.css";
+import { Reveal, Card, Button } from 'semantic-ui-react';
 
 const Flashcard = (props) => {
   let flashcard = props.flashcard;
   return (
-    <div className='flashcard' id={`id_${flashcard.id}`}>
-      <div className='flashcard-inner'>
-        <div className='flashcard-front' id={`question_${flashcard.id}`}>
-            <div className='front-text'>
-              {flashcard.question}
-            </div>
-        </div>
-        <div className='flashcard-back' id={`answer_${flashcard.id}`}>
-          <div className='back-text'>
+    <>
+      <Reveal animated='fade' id={`id_${flashcard.id}`}>
+        <Reveal.Content visible>
+          <Card style={{ width: 580, height: 400 }}>
+            <Card.Header as="h1" className="ui orange header" id={`question_${flashcard.id}`} style={{ marginTop: '5em' }} textAlign='center'>
+            {flashcard.question}
+            </Card.Header>
+          </Card>
+        </Reveal.Content>
+        <Reveal.Content>
+          <Card style={{ width: 580, height: 400 }}>
+            <Card.Header as="h3" className="ui black header" id={`answer_${flashcard.id}`} style={{ marginTop: '7em', marginLeft: '1em', marginRight: '1em'}} textAlign='center'>
             {flashcard.answer}
-          </div>
-        </div>
-      </div>
+            </Card.Header>
+          </Card>
+        </Reveal.Content>
+      </Reveal>
 
-      <div className='button-group'>
-        <button className='update-button' onClick={props.updateStatus} id='red'>Repeat, please</button>
-        <button className='update-button' onClick={props.updateStatus} id='yellow'>Needs more practice</button>
-        <button className='update-button' onClick={props.updateStatus} id='green'>I got this!</button>
-      </div>
-    </div>
+      <Button className='update-button' onClick={props.updateStatus} id='red'>
+        Repeat, please
+      </Button>
+      <Button className='update-button' onClick={props.updateStatus} id='yellow'>
+        Needs more practice
+      </Button>
+      <Button className='update-button' onClick={props.updateStatus} id='green'>
+        I got this
+      </Button>
+    </>
   )
 };
 
