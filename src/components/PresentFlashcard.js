e8e404f372a7ae94f31fe2f7c77d984f68068f9b
oliverochman <oliver.ochman@hotmail.com>
2019-09-09 16:53:12 +0200

    adds method to get new deck
    Co-authored-by: julieahlquist <julieahlquist@hotmail.com>
    Co-authored-by: dashlykova <daria.lykova@gmail.com>

--- src/components/PresentFlashcard.js
+++ src/components/PresentFlashcard.js
@@ -6,15 +6,43 @@ import Flashcard from "./Flashcard";
 export class PresentFlashcard extends Component {
   state = {
     flashcards: [],
-    activeFlashcard: 0
+    activeFlashcard: 0,
+    nextDeckPage: "nil"
   };
 
   async componentDidMount() {
     const response = await axios.get("http://localhost:3000/api/decks");
     this.setState({
-      flashcards: response.data.decks[0].flashcards
+      flashcards: response.data.decks[0].flashcards,
+      nextDeckPage: response.data.meta.nextPage
     });
   };
+
+  getNewDeck = async () => {
+    let page;
+    
+    if (this.state.nextDeckPage === "nil") {
+      page = 1
+    } else {
+      page = this.state.nextDeckPage
+    }
+
+    const response = await axios.get(`http://localhost:3000/api/decks/?page=${page}`);
+
+    this.setState({
+      flashcards: response.data.decks[0].flashcards,
+      activeFlashcard: 0,
+      nextDeckPage: response.data.meta.nextPage,
+      renderDeckOption: false
+    })
+  }
+
+  repeatCurrentDeck = () => {
+    this.setState({
+      activeFlashcard: 0,
+      renderDeckOption: false
+    })
+  }
 
   updateStatus = (event) => {
     let status = event.target.id
@@ -50,8 +78,8 @@ export class PresentFlashcard extends Component {
     if (this.state.renderDeckOption === true) {
       chooseDeckOption = (
         <>
-          <button id="repeat-deck">Repeat</button>
-          <button id="get-new-deck">New Deck</button>
+          <button onClick={() => this.repeatCurrentDeck()} id="repeat-deck">Repeat</button>
+          <button onClick={() => this.getNewDeck()} id="get-new-deck">New Deck</button>
         </>
       )
     }
