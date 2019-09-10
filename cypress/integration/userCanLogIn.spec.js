53a8b99444bf7cae1640a4b8294b03bf166e26bf
Julie Ahlquist <julieahlquist@hotmail.com>
2019-09-11 00:26:58 +0200

    adds cypress command for login and refactors userCanLogin acceptance test

--- cypress/integration/userCanLogIn.spec.js
+++ cypress/integration/userCanLogIn.spec.js
@@ -7,20 +7,14 @@ describe('User can login', () => {
       response: 'fixture:flashcards.json',
       status: 200
     });
-    cy.route({
-      method: "POST",
-      url: "http://localhost:3000/api/auth/sign_in",
-      response: "fixture:user-login.json"
-    });
-    cy.visit('http://localhost:3001');
   });
 
   it('Successful login', () => {
-    cy.get('#login-button').click();
-    cy.get('#login-form').within(() => {
-      cy.get('#email').type(email);
-      cy.get('#password').type(password);
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/api/auth/sign_in',
+      response: 'fixture:successful_user_login.json'
     });
-    cy.get('#submit-login-form').click();
+    cy.user_login('johndoe@email.com', 'password');
   });
 });
