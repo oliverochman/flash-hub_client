e6d66244a3cb788307259030c963d2ba6ada94d2
Julie Ahlquist <julieahlquist@hotmail.com>
2019-09-10 12:24:31 +0200

    moves test for category to visitorCanSeeFirstFlashcard and adds assertion tests for repeat or get new deck

--- cypress/integration/visitorCanSeeFirstFlashcard.spec.js
+++ cypress/integration/visitorCanSeeFirstFlashcard.spec.js
@@ -10,8 +10,9 @@ describe('Visitor can see first flashcard on the homepage', () => {
     cy.visit('http://localhost:3001');
   });
 
-  it('Correct content of flashcard is visible', () => {
+  it('has a category', () => {
     cy.get('#id_1').within(() => {
+      cy.get('#category_JavaScript').contains('JavaScript');
       cy.get('#question_1').contains('How can you include an external javascript file?');
       cy.get('#answer_1').contains("/script src='myfile.js'/");
     });
