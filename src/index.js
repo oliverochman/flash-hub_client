9e96ab1e630606df53ea6646c4d5b42e467e6baf
Julie Ahlquist <julieahlquist@hotmail.com>
2019-09-10 23:37:53 +0200

    configures redux-token-auth by creating and subscribing to store, creating rootReducer and actions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import configureStore from "./redux/store/configureStore";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.unregister();
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root')
+);
+
