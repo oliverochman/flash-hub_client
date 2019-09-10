887fe965048be34012dccf2aa37db8674e526718
Julie Ahlquist <julieahlquist@hotmail.com>
2019-09-11 00:29:39 +0200

    adds functionality to click on login button in navbar and get login form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu, Header } from 'semantic-ui-react';
+import { Menu, Header, Modal, Button, Form } from 'semantic-ui-react';
 import '../styling/customize.css';
 
 class Navbar extends Component {
@@ -16,14 +16,29 @@ class Navbar extends Component {
           Flashcard Hub
           </Header>
         <Menu.Menu position='right'>
+
+        <Modal trigger={
           <Menu.Item style={{ color: 'orange' }}
-            id='login-button'
-            name='login'
-            active={activeItem === 'login'}
-            onClick={this.handleItemClick}
+          id='login-button'
+          name='login'
+          active={activeItem === 'login'}
+          onClick={this.handleItemClick}
           >
             Log In
-          </Menu.Item>
+          </Menu.Item>}>
+          <Header>Log In</Header>
+          <Form id='login-form'>
+            <Form.Field>
+              <label>E-mail</label>
+              <input id='email' placeholder='E-mail' />
+            </Form.Field>
+            <Form.Field>
+              <label>Password</label>
+              <input id='password' placeholder='Password' type='password' />
+            </Form.Field>
+            <Button id='submit-login-form' type='submit'>Log In</Button>
+          </Form>
+        </Modal>
           <Menu.Item style={{ color: '#E58869' }}
             name='signup'
             active={activeItem === 'signup'}
