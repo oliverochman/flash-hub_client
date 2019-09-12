28fe7c4017a995bbe351add9142a297c2b9b75f1
Julie Ahlquist <julieahlquist@hotmail.com>
2019-09-12 11:24:23 +0200

    refactors acceptance test and fixes typo in fixture file
    Co-authored-by: Daria Lykova <daria.lykova@gmail.com>

--- cypress/integration/userCanGetSavedFlashcards.spec.js
+++ cypress/integration/userCanGetSavedFlashcards.spec.js
@@ -13,20 +13,24 @@ describe('User can get saved flashcards', () => {
       url: 'http://localhost:3000/api/auth/sign_in',
       response: 'fixture:successful_user_login.json',
       status: 200
+    })
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/api/saved_flashcards',
+      response: 'fixture:saved_flashcards.json',
+      status: 200
     });
-
     cy.visit('http://localhost:3001');
+    cy.user_login('julie@dash.com', 'password');
   });
 
   it('Chooses to see flashcards with status red', () => {
-  
-    cy.user_login('julie@dash.com', 'password');
 
-    cy.get('#my-flashcards-button').click();
+    cy.get('#my-flashcards-button').click()
     cy.get('#my-flashcards').within(() => {
       cy.get('#category_JavaScript').contains('JavaScript');
-      cy.get('#question_3').contains('How can you include an external javascript file?');
-      cy.get('#answer_3').contains("/script src='myfile.js'/");
+      cy.get('#saved_question_1').contains('How can you include an external javascript file?');
+      cy.get('#saved_answer_1').contains("/script src='myfile.js'/");
     })
   });
 });
