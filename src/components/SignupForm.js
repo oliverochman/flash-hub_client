22bf58b0049140bc4258d84643ca088397e1ca18
Julie Ahlquist <julieahlquist@hotmail.com>
2019-09-13 21:13:53 +0200

    styles signup button, form and title

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -48,42 +48,42 @@ class SignupForm extends Component {
       <>
         <Modal id='signup-modal' basic size='small' trigger={<Button id='signup-button'>Sign Up</Button>}>
           <Container>{flashMessage}</Container>
-        <h1 style={{ fontSize: '4rem', textAlign: 'center', fontFamily: 'Londrina Shadow' }}>
+        <h1 style={{ fontSize: '4rem', textAlign: 'center', fontFamily: 'Londrina Shadow' }}>
           Sign Up
         </h1>
           <Form id='signup-form' onSubmit={e => this.signupHandler(e)}>
-            <Form.Field>
-              <label id='email-title'>E-mail</label>
-              <input
-                id='email'
-                placeholder='E-mail'
-                onChange={e => this.setState({ email: e.target.value })}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label id='password-title'>Password</label>
-              <input
-                id='password' 
-                placeholder='Password' 
-                type='password'
-                onChange={e => this.setState({ password: e.target.value })}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label id='password-title'>Password confirmation</label>
-              <input
-                id='password_confirmation' 
-                placeholder='Password confirmation' 
-                type='password'
-                onChange={e => this.setState({ password_confirmation: e.target.value })}
-              />
-            </Form.Field>
-          <Button fluid color='orange' type='submit' id='submit-signup-form'>Sign Up</Button>
-          </Form>
-      </Modal>
-    </>
-    )
-  };
+            <Form.Field>
+              <label id='email-title'>E-mail</label>
+              <input
+                id='email'
+                placeholder='E-mail'
+                onChange={e => this.setState({ email: e.target.value })}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label id='password-title'>Password</label>
+              <input
+                id='password' 
+                placeholder='Password' 
+                type='password'
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label id='password-title'>Password confirmation</label>
+              <input
+                id='password_confirmation' 
+                placeholder='Password confirmation' 
+                type='password'
+                onChange={e => this.setState({ password_confirmation: e.target.value })}
+              />
+            </Form.Field>
+          <Button fluid color='orange' type='submit' id='submit-signup-form'>Sign Up</Button>
+        </Form>
+      </Modal>
+      </>
+    )
+  };
 };
 
 const mapStateToProps = state => {
