8b3d197b4a8f73a41a86868c0ab69bc90657fa07
Daria Lykova <daria.lykova@gmail.com>
2019-09-14 10:04:34 +0200

    Adds styling to log out button

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Menu } from 'semantic-ui-react';
 import { signOutUser } from '../state/actions/reduxTokenAuthConfig';
-// import AlertMessage from './AlertMessage'
 
 class Logout extends Component {
 
